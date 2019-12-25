19311d5afd18707cac4c30b5f51ff9dd90b1c368
matt <mholford@protonmail.com>
2019-12-25 03:49:18 -0800

    ==[Chapter 2]== Assigning variables Type inference Values Constants Kotlin -> JVM -> Java

---
+++ src/TypeIntro.kt
@@ -0,0 +1,9 @@
+const val MAX_EXPERIENCE: Int = 5000    //Constant. Must be declared outside functions and can only be basic type.
+
+fun main(args: Array<String>) {
+    val playerName = "Matt"             //Read-only variable
+    var experiencePoints = 5            //Assignment
+    experiencePoints += 5               //Addition and assignment
+    println(experiencePoints)
+    println(playerName)
+}
